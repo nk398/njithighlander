6cae8d5bd938aacd48364fa82cd81f5c9c3babc3
Neha Kazi <47070642+nk398@users.noreply.github.com>
2022-04-28 16:49:19 -0400

    Create coaches.js

---
+++ public/js/coaches.js
@@ -0,0 +1,485 @@
+
+//////////// Data structure: One object-array of coaches for each game
+
+//////////// m_baseball
+var m_baseball_coaches = [];
+m_baseball_coaches.push({
+"image": "https://njithighlanders.com/images/2019/8/19/LC1_1390_1.jpg?width=300",
+"name": "Robbie McClellan",
+"title": "Head Baseball Coach",
+"email": "rmcclell@njit.edu",
+"phone": "973-596-8396"});
+
+m_baseball_coaches.push({
+"image":"https://njithighlanders.com/images/2019/8/19/LC1_1396_3.jpg?width=300",
+"name": "Giuseppe Papaccio",
+"title": "Assistant Coach",
+"email": "papaccio@njit.edu",
+"phone": "973-596-5827"});
+
+m_baseball_coaches.push({ 
+"image": "https://njithighlanders.com/images/2019/8/19/LC1_1400_4_71.jpg?width=300",
+"name":"Anthony Deleo",
+"title": "Assistant Coach",
+"email": "anthony.j.deleo@njit.edu",
+"phone": "973-596-8532"});
+
+m_baseball_coaches.push({
+"image": "https://njithighlanders.com/images/2022/1/12/NJT_0002.jpg?width=300",
+"name": "Matthew Greely",
+"title": "Assistant Coach",
+"email": "MG835@NJIT.edu",
+"phone": ""});
+
+////////////m_basketball
+var m_basketball_coaches = [];
+m_basketball_coaches.push({
+image: "https://njithighlanders.com/images/2019/10/15/NJIT_Kennedy_Brian_71.JPG?width=300",
+name: "Brian Kennedy",
+title: "Head Coach",
+email: "bkennedy@njit.edu",
+phone: "973-596-5216"});
+
+m_basketball_coaches.push({
+image: "https://njithighlanders.com/images/2020/10/26/2020_10_21_MBB_0343.jpg?width=300",
+name: "Jeff Rafferty",
+title: "Assistant Coach",
+email: "jeffrey.t.rafferty@njit.edu",
+phone: "973-596-5216"});
+
+m_basketball_coaches.push({
+image: "https://njithighlanders.com/images/2019/10/15/NJIT_Gutowski_Joseph_71.JPG?width=300",
+name: "Joe Gutowski",
+title: "Assistant Coach",
+email: "gutowski@njit.edu",
+phone: "973-596-5336"});
+
+m_basketball_coaches.push({
+image: "https://njithighlanders.com/images/2021/7/6/Ricardo_Rush_Headshot.png?width=300",
+name: "Ricardo Rush Jr.",
+title: "Assistant Coach",
+email: "ricardo.l.rush@njit.edu",
+phone: ""});
+
+m_basketball_coaches.push({
+image: "https://njithighlanders.com/images/2019/10/15/NJIT_Manuel_Danny_71.JPG?width=300",
+name: "Danny Manuel",
+title: "Director of Basketball Operations",
+email: "daniel.r.manuel@njit.edu",
+phone: "973-596-5723"});
+
+
+//m_crosscountry
+var m_crosscountry_coaches = [];
+m_crosscountry_coaches.push({
+"image": "https://njithighlanders.com/images/2021/9/10/HC_Jason_Munsch.jpg?width=300",
+"name": "Jason Munsch",
+"title": "Head Cross Country/Track & Field Coach",
+"email": "jason.s.munsch@njit.edu",
+"phone": ""});
+
+m_crosscountry_coaches.push({
+"image": "https://njithighlanders.com/images/2021/11/2/AC_Chris_Lessard.jpg?width=300",
+"name": "Chris Lessard",
+"title": "Assistant Coach (Cross Country/Distance)",
+"email": "christopher.e.lessard@njit.edu",
+"phone": ""});
+
+//m_fencing
+var m_fencing_coaches = [];
+m_fencing_coaches.push({
+"image": "https://njithighlanders.com/images/2021/4/14/Jason_Henderson_New.JPG?width=300",
+"name": "Jason Henderson",
+"title": "Head Fencing Coach",
+"email": "jason.v.henderson@njit.edu",
+"phone": "848-203-1854"});
+
+m_fencing_coaches.push({
+"image": "https://njithighlanders.com/images/2019/10/11/Ben_Igoe_New.jpg?width=300",
+"name": "Ben Igoe",
+"title": "Assistant Coach - Saber",
+"email": "	benjamin.d.igoe@njit.edu",
+"phone": ""});
+
+m_fencing_coaches.push({
+"image": "https://njithighlanders.com/images/2019/10/11/Nick_Vomero2.JPG?width=300",
+"name": "Nick Vomero",
+"title": "Assistant Coach - Epee",
+"email": "",
+"phone": ""});
+
+m_fencing_coaches.push({
+"image": "https://njithighlanders.com/images/2021/10/25/Kevin_Cho.jpg?width=300",
+"name": "Kevin Cho",
+"title": "Assistant Coach - Foil",
+"email": "",
+"phone": ""});
+
+//m_lacrosse
+var m_lacrosse_coaches = [];
+m_lacrosse_coaches.push({
+"image": "https://njithighlanders.com/images/2014/2/17/TravisJohnsonMLax-HS-001.jpg?width=300",
+"name": "Travis Johnson",
+"title": "Head Men's Lacrosse Coach",
+"email": "lacrosse@njit.edu",
+"phone": "973-596-3615"});
+
+m_lacrosse_coaches.push({
+"image": "https://njithighlanders.com/images/2019/11/14/BlaseMantineo.jpg?width=300",
+"name": "Blase Mantineo",
+"title": "Assistant Coach",
+"email": "",
+"phone": ""});
+
+m_lacrosse_coaches.push({
+"image": "https://njithighlanders.com/images/2021/12/5/Cade_Siddron.jpg?width=300",
+"name": "Cade Siddron",
+"title": "Assistant Coach",
+"email": "",
+"phone": ""});
+
+m_lacrosse_coaches.push({
+"image": "https://njithighlanders.com/images/2021/1/22/Sam_Altiero.jpg?width=300",
+"name": "Sam Altiero",
+"title": "Volunteer Assistant Coach",
+"email": "",
+"phone": ""});
+
+//m_soccer
+var m_soccer_coaches = [];
+m_soccer_coaches.push({
+"image": "https://njithighlanders.com/images/2021/8/16/FERNANDO_BARBOTO.jpg?width=300",
+"name": "Fernando Barboto",
+"title": "Head Coach",
+"email": "barboto@njit.edu",
+"phone": "973-596-5219"});
+
+m_soccer_coaches.push({
+"image": "https://njithighlanders.com/images/2016/5/6/Matthew%20Uy.JPG?width=300",
+"name": "Matthew Uy",
+"title": "Assistant Coach",
+"email": "muy@njit.edu",
+"phone": "973-596-5226"});
+
+m_soccer_coaches.push({
+"image": "https://njithighlanders.com/images/2021/8/16/AUGI_FREITAS.jpg?width=300",
+"name": "Augi Freitas",
+"title": "Assistant Coach | Head Recruiter",
+"email": "afreitas@njit.edu",
+"phone": "973-596-5218"});
+
+m_soccer_coaches.push({
+"image": "https://njithighlanders.com/images/2022/4/6/Alex_Luna.jpg?width=300",
+"name": "Alex Luna",
+"title": "Goalkeer Coach",
+"email": "",
+"phone": ""});
+
+m_soccer_coaches.push({
+"image": "https://njithighlanders.com/images/2021/8/16/NICHOLAS_KOZDRON.jpg?width=300",
+"name": "Nicholas Kozdron",
+"title": "Graduate Assistant",
+"email": "",
+"phone": ""});
+
+//m_swimdive
+var m_swimdive_coaches = [];
+m_swimdive_coaches.push({
+"image": "https://njithighlanders.com/images/2018/10/16/FarinaRon.JPG?width=300",
+"name": "Ron Farina",
+"title": "Head Coach, Swimming & Diving",
+"email": "farina@njit.edu",
+"phone": "973-596-5229"});
+
+m_swimdive_coaches.push({
+"image": "https://njithighlanders.com/images/2019/10/8/Shawn_Bryan.jpg?width=300",
+"name": "Shawn Bryan",
+"title": "Assistant Coach, Swimming",
+"email": "shawn.m.bryan@njit.edu",
+"phone": "973-596-8429"});
+
+m_swimdive_coaches.push({
+"image": "https://njithighlanders.com/images/2019/10/8/DoraFyde.jpg?width=300",
+"name": "Dora Fyfe",
+"title": "Assistant Coach, Swimming/Head Diving Coach",
+"email": "fyfe@njit.edu",
+"phone": ""});
+
+//m_tennis
+var m_tennis_coaches = [];
+m_tennis_coaches.push({
+"image": "https://njithighlanders.com/images/2021/10/5/NJT_5337.JPG?width=300",
+"name": "Marco Matteucci",
+"title": "Head Coach - Men's Tennis",
+"email": "marco.j.matteucci@njit.edu",
+"phone": "973-596-8297"});
+
+m_tennis_coaches.push({
+"image": "https://njithighlanders.com/images/2021/10/5/NJT_5345.JPG?width=300",
+"name": "Aitor Reparaz",
+"title": "Graduate Assistant",
+"email": "ar223@njit.edu",
+"phone": ""});
+
+//m_trackfield
+var m_trackfield_coaches = [];
+m_trackfield_coaches.push({
+"image": "https://njithighlanders.com/images/2021/9/10/HC_Jason_Munsch.jpg?width=300",
+"name": "Jason Munsch",
+"title": "Head Cross Country/Track & Field Coach",
+"email": "jason.s.munsch@njit.edu",
+"phone": ""});
+
+m_trackfield_coaches.push({
+"image": "https://njithighlanders.com/images/2021/11/2/AC_Chris_Lessard.jpg?width=300",
+"name": "Chris Lessard",
+"title": "Assistant Coach (Cross Country/Distance)",
+"email": "christopher.e.lessard@njit.edu",
+"phone": ""});
+
+m_trackfield_coaches.push({
+"image": "https://njithighlanders.com/images/2021/11/12/AC_Anthony_Gotto.jpg?width=300",
+"name": "Anthony Gotto",
+"title": "Assistant Coach (Throws)",
+"email": "",
+"phone": ""});
+
+m_trackfield_coaches.push({
+"image": "https://njithighlanders.com/images/2021/9/10/VAC_John_Crombie.jpg?width=300",
+"name": "John Crombie",
+"title": "Volunteer Assistant Coach",
+"email": "",
+"phone": ""});
+
+//m_volleyball
+var m_volleyball_coaches = [];
+m_volleyball_coaches.push({
+"image": "https://njithighlanders.com/images/2021/12/10/DANNY_GONCALVES.jpg?width=300",
+"name": "Danny Goncalves",
+"title": "Head Men's Volleyball",
+"email": "danny.goncalves@njit.edu",
+"phone": "973-596-5380"});
+
+m_volleyball_coaches.push({
+"image": "https://njithighlanders.com/images/2021/12/10/DAN_REILLY.jpg?width=300",
+"name": "Dan Reilly",
+"title": "Assistant Coach",
+"email": "daniel.j.reilly@njit.edu",
+"phone": "973-596-8295"});
+
+m_volleyball_coaches.push({
+"image": "https://njithighlanders.com/images/2022/1/10/JABARRY_GOODRIDGE.jpg?width=300",
+"name": "Jabarry Goodridge",
+"title": "Assistant Coach",
+"email": "",
+"phone": ""});
+
+m_volleyball_coaches.push({
+"image": "https://njithighlanders.com/images/2022/2/9/IMG_9085.jpg?width=300",
+"name": "Garrett Minyard",
+"title": "Volunteer Director of Operations",
+"email": "",
+"phone": ""});
+
+
+//w_basketball
+var w_basketball_coaches = [];
+w_basketball_coaches.push({
+"image": "https://njithighlanders.com/images/2018/10/16/LaneMike.JPG?width=300",
+"name": "Mike Lane",
+"title": "Head Coach",
+"email": "mlane@njit.edu",
+"phone": "973-596-3633"});
+
+w_basketball_coaches.push({
+"image": "https://njithighlanders.com/images/2020/10/6/LT_Watson_Glasses.jpg?width=300",
+"name": "LaTonya Watson",
+"title": "Assistant Coach",
+"email": "lwatson@njit.edu",
+"phone": "973-596-3635"});
+
+w_basketball_coaches.push({
+"image": "https://njithighlanders.com/images/2018/10/16/GratchAaron.JPG?width=300",
+"name": "Aaron Gratch",
+"title": "Assistant Coach",
+"email": "agratch@njit.edu",
+"phone": "973-596-3634"});
+
+w_basketball_coaches.push({
+"image": "https://njithighlanders.com/images/2018/10/16/MitchellSara.JPG?width=300",
+"name": "Sara Mitchell abc",
+"title": "Assistant Coach",
+"email": "smitch@njit.edu",
+"phone": "973-596-8442"});
+
+w_basketball_coaches.push({
+"image": "https://njithighlanders.com/images/2020/12/9/AnyssaSanchez.jpg?width=300",
+"name": "Anyssa Sanchez",
+"title": "Director of Basketball Operations",
+"email": "anyssa.m.sanchez@njit.edu",
+"phone": "973-596-8332"});
+
+//w_crosscountry
+var w_crosscountry_coaches = [];
+w_crosscountry_coaches.push({
+"image": "https://njithighlanders.com/images/2021/9/10/HC_Jason_Munsch.jpg?width=300",
+"name": "Jason Munsch",
+"title": "Head Cross Country/Track & Field Coach",
+"email": "jason.s.munsch@njit.edu",
+"phone": ""});
+
+w_crosscountry_coaches.push({
+"image": "https://njithighlanders.com/images/2021/11/2/AC_Chris_Lessard.jpg?width=300",
+"name": "Chris Lessard",
+"title": "Assistant Coach (Cross Country/Distance)",
+"email": "christopher.e.lessard@njit.edu",
+"phone": ""});
+
+
+//w_fencing
+var w_fencing_coaches = [];
+w_fencing_coaches.push({
+"image": "https://njithighlanders.com/images/2021/4/14/Jason_Henderson_New.JPG?width=300",
+"name": "Jason Henderson",
+"title": "Head Fencing Coach",
+"email": "jason.v.henderson@njit.edu",
+"phone": "848-203-1854"});
+
+w_fencing_coaches.push({
+"image": "https://njithighlanders.com/images/2019/10/11/Ben_Igoe_New.jpg?width=300",
+"name": "Ben Igoe",
+"title": "Assistant Coach - Saber",
+"email": "benjamin.d.igoe@njit.edu",
+"phone": ""});
+
+w_fencing_coaches.push({
+"image": "https://njithighlanders.com/images/2019/10/11/Nick_Vomero2.JPG?width=300",
+"name": "Nick Vomero",
+"title": "Assistant Coach - Epee",
+"email": "",
+"phone": ""});
+
+w_fencing_coaches.push({
+"image": "https://njithighlanders.com/images/2021/10/25/Kevin_Cho.jpg?width=300",
+"name": "Kevin Cho",
+"title": "Assistant Coach - Foil",
+"email": "",
+"phone": ""});
+
+//w_soccer
+var w_soccer_coaches = [];
+w_soccer_coaches.push({
+"image": "https://njithighlanders.com/images/2017/8/3/Ally_Nick.JPG?width=300",
+"name": "Ally Nick",
+"title": "Head Coach",
+"email": "nick@njit.edu",
+"phone": "973-596-2928"});
+
+w_soccer_coaches.push({
+"image": "https://njithighlanders.com/images/2017/8/3/Mary_Hearin.JPG?width=300",
+"name": "Mary Hearin",
+"title": "Assistant Coach",
+"email": "hearin@njit.edu",
+"phone": "973-596-8428"});
+
+w_soccer_coaches.push({
+"image": "https://njithighlanders.com/images/2017/8/3/Chris_Duggan.JPG?width=300",
+"name": "Chris Duggan",
+"title": "Assistant Coach",
+"email": "",
+"phone": "973-596-8523"});
+
+w_soccer_coaches.push({
+"image": "https://njithighlanders.com/images/2016/8/30/Volunteer%20Coach%20Joe%20Nick.JPG?width=300",
+"name": "Joe Nick",
+"title": "Director of Operations",
+"email": "",
+"phone": ""});
+
+//w_tennis
+var w_tennis_coaches = [];
+w_tennis_coaches.push({
+"image": "https://njithighlanders.com/images/2021/11/8/Olaya_Garrido_Rivas.jpg?width=300",
+"name": "Olaya Garrido-Rivas",
+"title": "Head Women's Tennis Coach",
+"email": "olaya.garrido.rivas@njit.edu",
+"phone": ""});
+
+w_tennis_coaches.push({
+"image": "https://njithighlanders.com/images/2021/10/5/NJT_5345.JPG?width=300",
+"name": "Aitor Reparaz",
+"title": "M/W Tennis Graduate Assistant",
+"email": "ar223@njit.edu",
+"phone": ""});
+
+//w_trackfield
+var w_trackfield_coaches = [];
+w_trackfield_coaches.push({
+"image": "https://njithighlanders.com/images/2021/9/10/HC_Jason_Munsch.jpg?width=300",
+"name": "Jason Munsch",
+"title": "Head Cross Country/Track & Field Coach",
+"email": "jason.s.munsch@njit.edu",
+"phone": ""});
+
+w_trackfield_coaches.push({
+"image": "https://njithighlanders.com/images/2021/11/2/AC_Chris_Lessard.jpg?width=300",
+"name": "Chris Lessard",
+"title": "Assistant Coach (Cross Country/Distance)",
+"email": "christopher.e.lessard@njit.edu",
+"phone": ""});
+
+w_trackfield_coaches.push({
+"image": "https://njithighlanders.com/images/2021/11/12/AC_Anthony_Gotto.jpg?width=300",
+"name": "Anthony Gotto",
+"title": "Assistant Coach (Throws)",
+"email": "",
+"phone": ""});
+
+w_trackfield_coaches.push({
+"image": "https://njithighlanders.com/images/2021/9/10/VAC_John_Crombie.jpg?width=300",
+"name": "John Crombie",
+"title": "Volunteer Assistant Coach",
+"email": "",
+"phone": ""});
+
+//w_volleyball
+var w_volleyball_coaches = [];
+w_volleyball_coaches.push({
+"image": "https://njithighlanders.com/images/2021/8/24/Coach_Trevino.jpg?width=300",
+"name": "Pedro Trevino",
+"title": "Head Coach",
+"email": "pedro.trevino@njit.edu",
+"phone": "973-596-8582"});
+
+//m_bowling
+var m_bowling_coaches = [];
+
+//m_icehockey
+var m_icehockey_coaches = [];
+
+
+
+///// Create a Map  with key-value pairs
+///// key: Name of game.  value: object-array of coaches of the key game
+///// all_coaches.get("m_baseball") will return m_baseball_coaches
+
+const all_coaches  = new Map([
+  ["m_baseball", m_baseball_coaches],
+  ["m_basketball", m_basketball_coaches],
+  ["m_crosscountry", m_crosscountry_coaches],
+  ["m_fencing", m_fencing_coaches],
+  ["m_lacrosse", m_lacrosse_coaches],
+  ["m_soccer", m_soccer_coaches],
+  ["m_swimdive", m_swimdive_coaches],
+  ["m_tennis", m_tennis_coaches],
+  ["m_trackfield", m_trackfield_coaches],
+  ["m_volleyball", m_volleyball_coaches],
+  ["w_basketball", w_basketball_coaches],
+  ["w_crosscountry", w_crosscountry_coaches],
+  ["w_fencing", w_fencing_coaches],
+  ["w_soccer", w_soccer_coaches],
+  ["w_tennis", w_tennis_coaches],
+  ["w_trackfield", w_trackfield_coaches],
+  ["w_volleyball", w_volleyball_coaches]
+]);
+
